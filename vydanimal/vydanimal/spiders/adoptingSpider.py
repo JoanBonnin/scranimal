910f5284c67c68838aaf3f67362bf2e9e11da32d
Joan Bonnín <joan.bonnin@gmail.com>
2018-04-13 20:21:33 +0200

    master: fix bad selectors

--- vydanimal/vydanimal/spiders/adoptingSpider.py
+++ vydanimal/vydanimal/spiders/adoptingSpider.py
@@ -8,7 +8,6 @@ from twisted.internet.error import TimeoutError, TCPTimedOutError
 import sys
 
 class AdoptingSpider(scrapy.Spider):
-
     BOOLEAN_ATTRIBUTE_TRUE = 'SI'
     BOOLEAN_ATTRIBUTE_FALSE = 'NO'
     ATTRIBUTE_NA = 'NA'
@@ -67,10 +66,14 @@ class AdoptingSpider(scrapy.Spider):
         pet_container = response.css('div.ficha_animal')
 
         data = {
+            'dominio': self.get_domain(response),
+
             'id': self.extract_id(pet_container),
             'nombre': self.extract_name(pet_container),
+
             'estado': self.extract_status(pet_container),
             'urgente': self.extract_urgency(pet_container),
+
             'clase': self.extract_class(pet_container),
             'desde': self.extract_since(pet_container),
             'sexo': self.extract_gender(pet_container),
@@ -78,10 +81,13 @@ class AdoptingSpider(scrapy.Spider):
             'nacimiento': self.extract_birthday(pet_container),
             'raza': self.extract_race(pet_container),
             'tamano': self.extract_size(pet_container),
+            'peso': self.extract_weight(pet_container),
+            'chip': self.extract_chip(pet_container),
+            'situacion': self.extract_situation(pet_container),
             'localidad': self.extract_state(pet_container),
+
             'salud': self.extract_health(pet_container),
             'descripcion': self.extract_description(pet_container),
-            'dominio': self.get_domain(response),
         }
 
         for key, value in data.items():
@@ -91,38 +97,47 @@ class AdoptingSpider(scrapy.Spider):
 
     def extract_status(self, container):
         status = self.extract_with_css('strong.estado span::text', container)
-        if status is None:
-            return self.ATTRIBUTE_NA
         return status
+
+    def extract_special_case(self, container):
+        special_case = self.extract_with_css('strong.caso_especial span::text', container)
+        if special_case is not None:
+            special_case = True
+        else:
+            special_case = False
+        return special_case
 
     def extract_urgency(self, container):
         urgent = self.extract_with_css('strong.urgente span::text', container)
         if urgent is not None:
-            urgent = self.BOOLEAN_ATTRIBUTE_TRUE
+            urgent = True
         else:
-            urgent = self.BOOLEAN_ATTRIBUTE_FALSE
+            urgent = False
         return urgent
 
     # Create small function to avoid repeating code
     def extract_with_css(self, query, container):
         field = container.css(query).extract_first()
-
-        if field is None:
-            return self.ATTRIBUTE_NA
         return field
 
     def format_attribute(self, attr):
         if attr is None:
             return self.ATTRIBUTE_NA
 
-        attr = re.sub("\n\t\r", "", attr)
-        attr = re.sub("\s+", " ", attr) #Consecutive whitespaces
-        attr.strip()
+        if type(attr) is bool:
+            return self.BOOLEAN_ATTRIBUTE_TRUE if attr else self.BOOLEAN_ATTRIBUTE_FALSE
+
+        if type(attr) is str:
+            attr = re.sub("\n\t\r", "", attr)
+            attr = re.sub("\s+", " ", attr) #Consecutive whitespaces
+            attr.strip()
+            return attr
+
         return attr
 
     def get_domain(self, response):
         parsed_uri = urlparse(response.url)
-        domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+        domain = '{uri.netloc}'.format(uri=parsed_uri)
         return domain
 
     def try_multiple_css_selectors(self, queries, container):
@@ -148,21 +163,21 @@ class AdoptingSpider(scrapy.Spider):
 
     def extract_class(self, pet_container):
         _class = self.try_multiple_css_selectors(
-            ['.ficha_clase span::text', 'dd.ficha_clase::text'],
+            ['.ficha_tipo span::text', 'dd.ficha_tipo::text'],
             pet_container
         )
         return _class
 
     def extract_since(self, pet_container):
         since = self.try_multiple_css_selectors(
-            ['.ficha_since span::text', 'dd.ficha_since::text'],
+            ['.ficha_desde span::text', 'dd.ficha_desde::text'],
             pet_container
         )
         return since
 
     def extract_gender(self, pet_container):
         gender = self.try_multiple_css_selectors(
-            ['.ficha_desde span::text', 'dd.ficha_desde::text'],
+            ['.ficha_sexo span::text', 'dd.ficha_sexo::text'],
             pet_container
         )
         return gender
@@ -190,7 +205,28 @@ class AdoptingSpider(scrapy.Spider):
 
     def extract_size(self, pet_container):
         size = self.try_multiple_css_selectors(
-            ['.ficha_tamano span::text', 'dd.ficha_tamano::text'],
+            ['.ficha_tamanio span::text', 'dd.ficha_tamanio::text'],
+            pet_container
+        )
+        return size
+
+    def extract_weight(self, pet_container):
+        size = self.try_multiple_css_selectors(
+            ['.ficha_peso span::text', 'dd.ficha_peso::text'],
+            pet_container
+        )
+        return size
+
+    def extract_situation(self, pet_container):
+        size = self.try_multiple_css_selectors(
+            ['.ficha_situacion span::text', 'dd.ficha_situacion::text'],
+            pet_container
+        )
+        return size
+
+    def extract_chip(self, pet_container):
+        size = self.try_multiple_css_selectors(
+            ['.ficha_chip span::text', 'dd.ficha_chip::text'],
             pet_container
         )
         return size
@@ -210,8 +246,5 @@ class AdoptingSpider(scrapy.Spider):
         return health
 
     def extract_description(self, pet_container):
-        desc = self.try_multiple_css_selectors(
-            ['.ficha_descripcion div::text', 'dd.ficha_descripcion div::text'],
-            pet_container
-        )
+        desc = self.extract_with_css('.ficha_descripcion > div *::text', pet_container)
         return desc
